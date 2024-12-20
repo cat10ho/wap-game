40a9911675598aaa3e5688b886c98f0cce8b1c8b
이이삭 <dlrltn1091@gmail.com>
2024-12-20 13:59:33 +0900

    Revert "asd"
    This reverts commit 5487fbf9fa57b1f5e3e904769dcefd47b2cf8da9.

--- src/init/socket.js
+++ src/init/socket.js
@@ -4,15 +4,9 @@ import registerHandler from '../handlers/register.handler.js';
 //소켓 서버 연결 함수.
 
 const initSocket = (server) => {
-  // socket.io에 CORS 설정 추가
-  const io = new SocketIO(server, {
-    cors: {
-      origin: '*',  // 허용할 도메인 (여기서는 모든 도메인 허용)
-      methods: ['GET', 'POST'],
-    }
-  });
-  
-  // 소켓 핸들러 등록
+  const io = new SocketIO();
+  io.attach(server);
+
   registerHandler(io);
 };
 
